56392dc8d7ef1a69ea6f06a116731d1202760b3a
ArHiFranki <franki550@gmail.com>
2022-08-29 20:10:01 +0700

    Added simple player move controller based on player input

--- Assets/Scripts/Player/MoveController.cs
+++ Assets/Scripts/Player/MoveController.cs
@@ -2,14 +2,51 @@ using UnityEngine;
 
 namespace Softweather.Player
 {
+    [RequireComponent(typeof(Rigidbody))]
     public class MoveController : MonoBehaviour
     {
+        [SerializeField] private float moveSpeed = 10f;
+        [SerializeField] private float rigidbodyDrag = 6f;
+        [SerializeField] private float movementMultiplier = 10f;
+
+        private Vector3 moveDirection;
         private Vector2 playerMoveInput;
+        private Rigidbody myRigidbody;
+
+        private void Awake()
+        {
+            myRigidbody = GetComponent<Rigidbody>();
+        }
+
+        private void Start()
+        {
+            myRigidbody.freezeRotation = true;
+        }
+
+        private void Update()
+        {
+            DragControl();
+        }
+
+        private void FixedUpdate()
+        {
+            MovePlayer();
+        }
 
         public void SetMoveInput(Vector2 moveInput)
         {
             playerMoveInput = moveInput;
-            //Debug.Log("playerMoveInput: " + moveInput);
+            moveDirection = transform.forward * playerMoveInput.y + transform.right * playerMoveInput.x;
+        }
+
+        private void MovePlayer()
+        {
+            myRigidbody.AddForce(moveDirection.normalized * moveSpeed * movementMultiplier, ForceMode.Acceleration);
+        }
+
+        private void DragControl()
+        {
+            myRigidbody.drag = rigidbodyDrag;
         }
     }
 }
