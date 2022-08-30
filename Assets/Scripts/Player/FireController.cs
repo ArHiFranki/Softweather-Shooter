5d13d27ec63d9ea0c55eac2964e4b33cf202a9b4
ArHiFranki <franki550@gmail.com>
2022-08-30 21:49:42 +0700

    Added fire controller, import weapon model, hit VFX and muzzle flash VFX

--- Assets/Scripts/Player/FireController.cs
+++ Assets/Scripts/Player/FireController.cs
@@ -1,12 +1,70 @@
+using System.Collections;
 using UnityEngine;
 
 namespace Softweather.Player
 {
     public class FireController : MonoBehaviour
     {
+        [SerializeField] private Camera playerCamera;
+        [SerializeField] private float range = 100f;
+        [SerializeField] private float damage = 30f;
+        [SerializeField] private float timeBetweenShots = 0.5f;
+        [SerializeField] private ParticleSystem muzzleFlash;
+        [SerializeField] private GameObject hitEffect;
+
+        private bool canShoot = true;
+
+        private void Start()
+        {
+            canShoot = true;
+        }
+
         public void Fire()
         {
             //Debug.Log("Fire!");
+            if (canShoot)
+            {
+                StartCoroutine(Shoot());
+            }
+        }
+
+        private IEnumerator Shoot()
+        {
+            canShoot = false;
+            PlayMuzzleFlash();
+            ProcessRaycast();
+            yield return new WaitForSeconds(timeBetweenShots);
+            canShoot = true;
+        }
+
+        private void PlayMuzzleFlash()
+        {
+            muzzleFlash.Play();
+        }
+
+        private void ProcessRaycast()
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out hit, range))
+            {
+                CreateHitImpact(hit);
+
+                //if (hit.transform.TryGetComponent(out EnemyHealth target))
+                //{
+                //    target.TakeDamage(damage);
+                //}
+                Debug.Log("hit: " + hit.transform.name);
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        private void CreateHitImpact(RaycastHit hit)
+        {
+            GameObject impact = Instantiate(hitEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impact, 0.1f);
         }
     }
 }
