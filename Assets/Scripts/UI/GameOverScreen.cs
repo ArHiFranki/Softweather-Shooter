a7f6649de2f2bbd1c1051e8991d31f941d1d218d
ArHiFranki <franki550@gmail.com>
2022-09-04 18:14:52 +0700

    Added menu button to game over screen

--- Assets/Scripts/UI/GameOverScreen.cs
+++ Assets/Scripts/UI/GameOverScreen.cs
@@ -7,17 +7,20 @@ namespace Softweather.UI
     public class GameOverScreen : MonoBehaviour
     {
         [SerializeField] private Button restartButton;
+        [SerializeField] private Button menuButton;
         [SerializeField] private Button exitButton;
 
         private void OnEnable()
         {
             restartButton.onClick.AddListener(OnRestartButtonClick);
+            menuButton.onClick.AddListener(OnMenuButtonClick);
             exitButton.onClick.AddListener(OnExitButtonClick);
         }
 
         private void OnDisable()
         {
             restartButton.onClick.RemoveListener(OnRestartButtonClick);
+            menuButton.onClick.RemoveListener(OnMenuButtonClick);
             exitButton.onClick.RemoveListener(OnExitButtonClick);
         }
 
@@ -25,6 +28,12 @@ namespace Softweather.UI
         {
             int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
             SceneManager.LoadScene(currentSceneIndex);
+            Time.timeScale = 1;
+        }
+
+        private void OnMenuButtonClick()
+        {
+            SceneManager.LoadScene(ScenesNames.MenuScene);
             Time.timeScale = 1;
         }
 
