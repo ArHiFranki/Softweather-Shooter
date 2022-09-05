1ea9c7387bc8a323118db4d237db1d2a9e9999fe
ArHiFranki <franki550@gmail.com>
2022-09-05 15:46:26 +0700

    Added fire sound FX

---
+++ Assets/Scripts/Core/SoundFXController.cs
@@ -0,0 +1,37 @@
+using System.Threading;
+using UnityEngine;
+
+namespace Softweather.Core
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class SoundFXController : MonoBehaviour
+    {
+        [SerializeField] private AudioClip fireSound;
+        [SerializeField] private float fireSoundVolume = 1f;
+
+        private bool isSoundMuted;
+        private AudioSource myAudioSource;
+
+        private void Awake()
+        {
+            myAudioSource = GetComponent<AudioSource>();
+        }
+
+        private void Start()
+        {
+            LoadSettings();
+        }
+
+        public void PlayFireSound()
+        {
+            if (isSoundMuted) return;
+
+            myAudioSource.PlayOneShot(fireSound, fireSoundVolume);
+        }
+
+        private void LoadSettings()
+        {
+            isSoundMuted = PlayerPrefs.GetInt(PlayerPrefsValues.IsMuted) == 1;
+        }
+    }
+}
